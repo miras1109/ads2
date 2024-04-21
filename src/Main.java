04c4f0304bcc9b847227d24a906bf642a3870512
miras1109 <161669886+miras1109@users.noreply.github.com>
2024-04-21 22:38:17 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-                // Testing MyArrayList
                 MyList<Integer> arrayList = new MyArrayList<>();
                 arrayList.add(9);
                 arrayList.add(6);
                 arrayList.addFirst(3);
                 System.out.println("ArrayList: " + arrayList.toArray()[0] + ", " + arrayList.toArray()[1] + ", " + arrayList.toArray()[2]);
 
-                // Testing MyLinkedList
                 MyList<Integer> linkedList = new MyLinkedList<>();
                 linkedList.add(1);
                 linkedList.add(4);
